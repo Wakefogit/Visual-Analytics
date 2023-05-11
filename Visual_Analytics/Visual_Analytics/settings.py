0b38c99c79bd223ac939fd28e9e325ddd4e5ce98
LENOVO@DESKTOP <pranitbhoy68@.gmail.com>
2023-05-11 18:09:59 +0530

    database added

--- Visual_Analytics/Visual_Analytics/settings.py
+++ Visual_Analytics/Visual_Analytics/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+TEMP=Path.joinpath(BASE_DIR,"templates")
+STATIC=Path.joinpath(BASE_DIR,"static")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'login'
 ]
 
 MIDDLEWARE = [
@@ -73,13 +74,25 @@ WSGI_APPLICATION = "Visual_Analytics.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default':{
+        'ENGINE':'mssql',
+        'NAME':'flow',
+        'USER':'',                     # Keep it blank
+        'PASSWORD':'',                 # Keep it blank
+        'HOST':'localhost\SQLEXPRESS',
+        # 'PORT':'',                   # Can be removed
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
